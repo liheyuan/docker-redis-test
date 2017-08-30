b737f6ad01238f43ae8ff8181f13a1a3755e1d5c
coder4 <lihy@fenbi.com>
2017-08-30 15:54:57 +0800

    MOD: Support password

--- run_redis_docker.sh
+++ run_redis_docker.sh
@@ -2,7 +2,8 @@
 NODE_NAME="node-redis"
 DOCKER_NAME="sbmvt-redis"
 VOLUME_DATA='/var/lib/boot2docker/docker_data/redis'
+PASSWORD="hehehe"
 eval $(docker-machine env $NODE_NAME)
 last_id=$(docker ps -l -q)
 docker rm -f $last_id
-docker run --rm --name $DOCKER_NAME -v "$VOLUME_DATA":/data -p6379:6379 -d redis:4-alpine redis-server --appendonly yes
+docker run --rm --name $DOCKER_NAME -v "$VOLUME_DATA":/data -p6379:6379 -d redis:4-alpine redis-server --appendonly yes --requirepass $PASSWORD 
